25fcebca4300749a0de69abfba684fdc1cc2d099
IltisAuge <danielziegler.1@gmx.de>
2023-07-27 15:43:02 +0200

    Updates

---
+++ precache-manifest.5d439f66b3e99ddec89fea6c799ed0f4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "15f7295ebf049b82347c",
+    "url": "/iltisauge-website/static/css/main.a746bfd5.chunk.css"
+  },
+  {
+    "revision": "15f7295ebf049b82347c",
+    "url": "/iltisauge-website/static/js/main.15f7295e.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/iltisauge-website/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "6422faee7361a7d7a3d5",
+    "url": "/iltisauge-website/static/js/2.6422faee.chunk.js"
+  },
+  {
+    "revision": "87ff16c4263a99474089",
+    "url": "/iltisauge-website/static/js/runtime~main.87ff16c4.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/iltisauge-website/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "5302bf976d531b71e8b64ba75f17ef75",
+    "url": "/iltisauge-website/index.html"
+  }
+];
